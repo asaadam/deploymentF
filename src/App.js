06abe26ee26ace92d9544865dfaba40ce0161010
asaadam <graha.asa@gmail.com>
2021-09-26 11:51:20 +0700

    fix: unused useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
-import { useState } from "react";
 import { Form } from "./component/form";
 
 function App() {
-
   return (
     <div className='App'>
       <Form onSubmit={(data) => console.log(data)} />
